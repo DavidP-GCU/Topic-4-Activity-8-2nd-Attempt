875f05d7d7035e8d94032657ed313cdb54d82f54
DavidP-GCU <114124010+DavidP-GCU@users.noreply.github.com>
2022-10-09 19:51:36 -0600

    Add files via upload

---
+++ Activity 8 (2nd Attempt)/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Activity_8__2nd_Attempt_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fatTextBox = new System.Windows.Forms.TextBox();
+            this.carbTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.fatCaloriesLabel = new System.Windows.Forms.Label();
+            this.carbCaloriesLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // fatTextBox
+            // 
+            this.fatTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fatTextBox.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fatTextBox.Location = new System.Drawing.Point(300, 33);
+            this.fatTextBox.Name = "fatTextBox";
+            this.fatTextBox.Size = new System.Drawing.Size(100, 26);
+            this.fatTextBox.TabIndex = 0;
+            this.fatTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // carbTextBox
+            // 
+            this.carbTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.carbTextBox.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.carbTextBox.Location = new System.Drawing.Point(300, 94);
+            this.carbTextBox.Name = "carbTextBox";
+            this.carbTextBox.Size = new System.Drawing.Size(100, 26);
+            this.carbTextBox.TabIndex = 1;
+            this.carbTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter fat in grams: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(229, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter fat in Carbs: ";
+            // 
+            // btnExit
+            // 
+            this.btnExit.AutoSize = true;
+            this.btnExit.BackColor = System.Drawing.SystemColors.Window;
+            this.btnExit.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(285, 226);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 25);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.AutoSize = true;
+            this.calculateButton.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.Location = new System.Drawing.Point(77, 226);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(89, 23);
+            this.calculateButton.TabIndex = 7;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.AutoSize = true;
+            this.clearButton.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(188, 226);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 8;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // fatCaloriesLabel
+            // 
+            this.fatCaloriesLabel.AutoSize = true;
+            this.fatCaloriesLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.fatCaloriesLabel.Font = new System.Drawing.Font("Lucida Console", 9.75F);
+            this.fatCaloriesLabel.Location = new System.Drawing.Point(38, 141);
+            this.fatCaloriesLabel.Name = "fatCaloriesLabel";
+            this.fatCaloriesLabel.Size = new System.Drawing.Size(2, 15);
+            this.fatCaloriesLabel.TabIndex = 9;
+            // 
+            // carbCaloriesLabel
+            // 
+            this.carbCaloriesLabel.AutoSize = true;
+            this.carbCaloriesLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.carbCaloriesLabel.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.carbCaloriesLabel.Location = new System.Drawing.Point(38, 188);
+            this.carbCaloriesLabel.Name = "carbCaloriesLabel";
+            this.carbCaloriesLabel.Size = new System.Drawing.Size(2, 15);
+            this.carbCaloriesLabel.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(445, 264);
+            this.Controls.Add(this.carbCaloriesLabel);
+            this.Controls.Add(this.fatCaloriesLabel);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.carbTextBox);
+            this.Controls.Add(this.fatTextBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Calories from Fats and Carbohydrates";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fatTextBox;
+        private System.Windows.Forms.TextBox carbTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label fatCaloriesLabel;
+        private System.Windows.Forms.Label carbCaloriesLabel;
+    }
+}
+
---
+++ Activity 8 (2nd Attempt)/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_8__2nd_Attempt_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Method used to calculate calories from fat grams. Method's name: "FatCalories()". 
+        private double FatCalories(double gramsofFat)
+        {
+            return gramsofFat * 9;
+        }
+
+        //Method used to calculate calories from carbohydrate grams. Method's name: "CarbCalories()".
+        private double CarbCalories(double gramsOfCarbs)
+        {
+            return gramsOfCarbs * 4;
+        }
+
+        //Method used to clear the user input (grams of fat and carbs) Method's name: "ClearAll()"
+        private void ClearAll()
+        {
+            fatTextBox.Text = "";
+            carbTextBox.Text = "";
+            fatCaloriesLabel.Text = "";
+            carbCaloriesLabel.Text = "";
+            fatTextBox.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e) //Event handler for Exit button. This button exits the windows from. 
+        {
+            this.Close();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e) //Event handler for Calculate Button. 
+        {
+            //Create double variables. 
+            double fatGrams = 0;
+            double carbGrams = 0;
+            double caloriesFromFat = 0;
+            double caloriesFromCarbs = 0;
+            
+
+            //Validate the users input (beginning of exception handler). 
+            try
+            {
+                //Get the number of fat grams
+                fatGrams = double.Parse(fatTextBox.Text);
+
+                //Get the number of carb grams
+                carbGrams = double.Parse(carbTextBox.Text);
+
+                //Calculate the number of calories from fat grams
+                caloriesFromFat = FatCalories(fatGrams);
+
+                //Calculate the number of calories from carb grams. 
+                caloriesFromCarbs = CarbCalories(carbGrams);
+
+                //Display the number of calories from fat grams
+                fatCaloriesLabel.Text = "Your number of calories from fat grams is: " + caloriesFromFat.ToString();
+
+                //Display the number of calories from carb grams. 
+                carbCaloriesLabel.Text = "Your number of calories from carb grams is: " + caloriesFromCarbs.ToString();
+
+                //Clear the input from the user. 
+                clearButton.Focus();
+
+
+              //If the user entered invalid input (the end of the exception handler). 
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                ClearAll(); //This clears the user's input by jumping to method named "ClearAll()". 
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e) //Event handler for Clear Button. 
+        {
+            ClearAll(); //This button clears the all information on windows form. 
+        }
+    }
+
+
+}
